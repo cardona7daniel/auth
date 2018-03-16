4963dabff46173ac83bcfc05962ac452635813b1
cardona7daniel <cardona7daniel@gmail.com>
2018-03-16 10:01:36 -0500

    Subida de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+
+//Rutas
+import { app_routing } from './app.routes';
+
+
+// Servicios
+import { AuthService } from './services/auth/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PreciosComponent,
+    ProtegidaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing,
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuardService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { AuthGuardService } from './services/auth-guard.service';
+
+
+const app_routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'precio', component: PreciosComponent },
+  {
+    path: 'protegida', component: ProtegidaComponent,
+    canActivate: [ AuthGuardService ]
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
+
+export const app_routing = RouterModule.forRoot(app_routes);
